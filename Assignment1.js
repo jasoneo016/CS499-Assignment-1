e06adef7a8039fd7e1c54cf9bf09ec15176085e8
Jason Zhang <jasoneo016@gmail.com>
2017-01-12 17:37:00 -0800

    Initial commit

---
+++ Assignment1.js
@@ -0,0 +1,14 @@
+/**
+ * Created by admin on 1/12/17.
+ */
+var express = require('express')
+var app = express()
+
+app.get('/', function (req, res) {
+   res.sendfile('index.html')
+})
+
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!')
+})
